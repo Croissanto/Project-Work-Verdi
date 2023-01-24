8591b9c95728782ca48789ec165ed4ae95b35cb6
Antonio Tallo <tallo.antonio1998@gmail.com>
2023-01-24 18:05:05 +0100

    Creazione entity
    -Create le prime due entità Author e LibriGame

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/Author.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/Author.java
@@ -1,5 +1,92 @@
 package com.example.demo.model;
 
-public class Author {
+import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Author {
+	
+	@Id
+	@SequenceGenerator(
+			name = "author_sequence", 
+			sequenceName ="author_sequence", 
+			allocationSize =1
+			)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_sequence")
+	private int id;
+	private String name;
+	private String surname;
+	private Date dataDiNascita;
+	private String luogoDiNascita;
+	
+	public Author() {
+		
+	}
+
+	public Author(int id, String name, String surname, Date dataDiNascita, String luogoDiNascita) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.dataDiNascita = dataDiNascita;
+		this.luogoDiNascita = luogoDiNascita;
+	}
+
+	public Author(String name, String surname, Date dataDiNascita, String luogoDiNascita) {
+		this.name = name;
+		this.surname = surname;
+		this.dataDiNascita = dataDiNascita;
+		this.luogoDiNascita = luogoDiNascita;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public String getLuogoDiNascita() {
+		return luogoDiNascita;
+	}
+
+	public void setLuogoDiNascita(String luogoDiNascita) {
+		this.luogoDiNascita = luogoDiNascita;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", name=" + name + ", surname=" + surname + ", dataDiNascita=" + dataDiNascita
+				+ ", luogoDiNascita=" + luogoDiNascita + "]";
+	}
+	
 }
--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/LibriGame.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/LibriGame.java
@@ -1,5 +1,107 @@
 package com.example.demo.model;
 
-public class LibriGame {
+import com.example.demo.model.enums.Genre;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class LibriGame {
+	
+
+	@Id
+	@SequenceGenerator(
+			name = "libri_sequence", 
+			sequenceName ="libri_sequence", 
+			allocationSize =1
+			)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "libri_sequence")
+	private int id;
+	private String title;
+	@ManyToOne
+	private Author autore;
+	private String publisher;
+	private int annoDiPubblicazione;
+	private Genre genre;
+	
+	public LibriGame() {
+		
+	}
+
+	public LibriGame(int id, String title, Author autore, String publisher, int annoDiPubblicazione, Genre genre) {
+		this.id = id;
+		this.title = title;
+		this.autore = autore;
+		this.publisher = publisher;
+		this.annoDiPubblicazione = annoDiPubblicazione;
+		this.genre = genre;
+	}
+
+	public LibriGame(String title, Author autore, String publisher, int annoDiPubblicazione, Genre genre) {
+		this.title = title;
+		this.autore = autore;
+		this.publisher = publisher;
+		this.annoDiPubblicazione = annoDiPubblicazione;
+		this.genre = genre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAutore() {
+		return autore;
+	}
+
+	public void setAutore(Author autore) {
+		this.autore = autore;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getAnnoDiPubblicazione() {
+		return annoDiPubblicazione;
+	}
+
+	public void setAnnoDiPubblicazione(int annoDiPubblicazione) {
+		this.annoDiPubblicazione = annoDiPubblicazione;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		return "LibriGame [id=" + id + ", title=" + title + ", autore=" + autore + ", publisher=" + publisher
+				+ ", annoDiPubblicazione=" + annoDiPubblicazione + ", genre=" + genre + "]";
+	}
+	
+	
 }
