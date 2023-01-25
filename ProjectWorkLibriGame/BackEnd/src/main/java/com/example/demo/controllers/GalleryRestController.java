0a9bb903e877aeb40dd411360ece4a394ae646b1
Biedr <pietrococozza1@gmail.com>
2023-01-25 17:40:31 +0100

    Creato rest e dto di Gallery
    Implementato la funzione create della restController e aggiustata classe GalleryDTO

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/controllers/GalleryRestController.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/controllers/GalleryRestController.java
@@ -1,8 +1,49 @@
 package com.example.demo.controllers;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.CreateGalleryDTO;
+import com.example.demo.model.Gallery;
+import com.example.demo.model.LibriGame;
+import com.example.demo.model.repositories.GalleryRepository;
+import com.example.demo.model.repositories.LibriGameRepository;
 
 @RestController
 public class GalleryRestController {
+	@Autowired
+	private GalleryRepository repoG;
+	@Autowired
+	private LibriGameRepository repoL;
+	
+	@PostMapping("/creategallery")
+	public boolean createGallery(@RequestBody CreateGalleryDTO dtoG) {
+			try {
+		List<LibriGame> list = new LinkedList<>();
+		for (Integer id : dtoG.getIdLibriGame()) {
+			Optional<LibriGame> opt2 = repoL.findById(id);
+			if(opt2.isPresent()) {
+				list.add(opt2.get());
+			}
+			Gallery gallery = new Gallery(dtoG.getTitoloSaga(), list,dtoG.getNumeroLibri());
+			repoG.save(gallery);
+		}
+			return true;
+		}catch(Exception e) {
+			
+			return false;
+		}
+		}
+	}
 
-}
+		
+	
+
+
+
---
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/dto/CreateGalleryDTO.java
@@ -0,0 +1,63 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class CreateGalleryDTO {
+	private int idLibroGame;
+	private String titoloSaga;
+	private int numeroLibri;
+	private List<Integer> idLibriGame;
+
+	public CreateGalleryDTO() {
+
+	}
+
+	public CreateGalleryDTO(int idLibroGame, String titoloSaga, int numeroLibri, List<Integer> idLibriGame) {
+		super();
+		this.idLibroGame = idLibroGame;
+		this.titoloSaga = titoloSaga;
+		this.numeroLibri = numeroLibri;
+		this.idLibriGame = idLibriGame;
+	}
+
+	public int getIdLibroGame() {
+		return idLibroGame;
+	}
+
+	public void setIdLibroGame(int idLibroGame) {
+		this.idLibroGame = idLibroGame;
+	}
+
+	public String getTitoloSaga() {
+		return titoloSaga;
+	}
+
+	public void setTitoloSaga(String titoloSaga) {
+		this.titoloSaga = titoloSaga;
+	}
+
+	public int getNumeroLibri() {
+		return numeroLibri;
+	}
+
+	public void setNumeroLibri(int numeroLibri) {
+		this.numeroLibri = numeroLibri;
+	}
+
+	public List<Integer> getIdLibriGame() {
+		return idLibriGame;
+	}
+
+	public void setIdLibriGame(List<Integer> idLibriGame) {
+		this.idLibriGame = idLibriGame;
+	}
+
+	@Override
+	public String toString() {
+		return "CreateGalleryDTO [idLibroGame=" + idLibroGame + ", titoloSaga=" + titoloSaga + ", numeroLibri="
+				+ numeroLibri + ", idLibriGame=" + idLibriGame + "]";
+	}
+	
+
+
+}
