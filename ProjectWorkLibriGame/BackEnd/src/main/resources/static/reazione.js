3cfbd3fb85caa7886823880312ff702991805b78
Croissanto <antoniopapaleo24@gmail.com>
2023-01-28 14:43:46 +0100

    Modifiche reazione ancora da sistemare

--- ProjectWorkLibriGame/BackEnd/src/main/resources/static/reazione.js
+++ ProjectWorkLibriGame/BackEnd/src/main/resources/static/reazione.js
@@ -1,92 +1,110 @@
 var likeCount = 0;
 var dislikeCount = 0;
 
+
+
 document.body.onload = () => {
-	
-	
-	stampa(1);
-	
-	
+
+
+
 }
 
 
 function addLike() {
-	
+	var id = document.getElementById("post").value;
 	likeCount++;
-	let data={
-		upVote: likeCount ,
+	let data = {
+		upVote: likeCount,
 		downVote: dislikeCount,
-		idUser: user.value,
-		idPost: post.value 
-       
-	}
-	
-	fetch('/createreazione', {
-  		method: 'POST',
-  		headers: {
-    		'Content-Type': 'application/json',
-  		},
-  		body: JSON.stringify(data),
-	}) .then((response) =>{
-        return response.json();
-    })
-    .then((data) =>{
-        console.log(data);
-    })
-    .then(() => {
-		
-		stampa(1);
-	});
+		idPost: post.value
 
-	
+
+	}
+
+	fetch('/createreazione', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	}).then((response) => {
+		return response.json();
+	})
+		.then((data) => {
+			console.log(data);
+		})
+		.then(() => {
+			console.log(id)
+			stampa();
+		});
+
+
 }
 
 function addDislike() {
-	
-	
+
 	dislikeCount++;
-	let data={
-		upvote: likeCount ,
+	let data = {
+		upvote: likeCount,
 		downvote: dislikeCount,
-		user: user.value,
-		post: post.value 
-       
+		idPost: post.value
+
+
 	}
-	
+
 	fetch('/createreazione', {
-  		method: 'POST',
-  		headers: {
-    		'Content-Type': 'application/json',
-  		},
-  		body: JSON.stringify(data),
-	})  .then((response)=> {
-        return response.json();
-    })
-    .then((data)=> {
-        console.log(data);
-    })
-    .then(() => {
-		
-		stampa(1);
-	});
-    
-    
-  }  
-function stampa(id) {
-	fetch(`http://localhost:8080/getreazionebypost/`+id).then((r) => { return r.json() })
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	}).then((response) => {
+		return response.json();
+	})
+		.then((data) => {
+			console.log(data);
+		})
+		.then(() => {
+
+			stampa();
+		});
+
+
+}
+
+
+function stampa() {
+
+
+	fetch('http://localhost:8080/getAllOrderByDateAndTime').then((r) => { return r.json() })
 		.then((r) => {
-			
+
+			postList.innerHTML = "";
+			for (let tmp of r) {
+				let elem = document.createElement("li");
+				elem.innerHTML = tmp.contenuto + " " + tmp.date + " " + tmp.time;
+
+				postList.appendChild(elem);
+			}
+		})
+		.then((r) => {
+var input = r.id;
+			return fetch('http://localhost:8080/getreazionebypost/' + input)
+		})
+
+		.then((r) => { return r.json() })
+
+		.then((r) => {
+
 			span1.innerHTML = "";
 			span2.innerHTML = '';
-			for (let reazione of r) {
-				
+			for (let reazione of r)
+
 				span1.innerHTML = reazione.upvote;
-				span2.innerHTML = reazione.downvote;
-				//elem.setAttribute("onclick","showPost("+tmp.id+")");			
-			}		
+			span2.innerHTML = reazione.downvote;
 		});
-} 
-   	
+}
+
 
 
 
