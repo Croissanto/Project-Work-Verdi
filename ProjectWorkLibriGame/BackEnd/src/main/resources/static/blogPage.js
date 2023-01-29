4d64c4c10cbdbfea79006f26e1192d6919bfc545
Antonio Tallo <tallo.antonio1998@gmail.com>
2023-01-29 17:07:50 +0100

    Aggiunta possibilità di commentare
    Va implementato poi nella pagina singola di ogni thread.

--- ProjectWorkLibriGame/BackEnd/src/main/resources/static/blogPage.js
+++ ProjectWorkLibriGame/BackEnd/src/main/resources/static/blogPage.js
@@ -11,7 +11,7 @@ function showAll() {
 			threadList.innerHTML = "";
 			for (let tmp of r) {
 				let elem = document.createElement("li");
-				elem.innerHTML = "<a href=/"+tmp.tema+".html>"+tmp.titolo+"</a>";
+				elem.innerHTML = "<a href=/" + tmp.tema + ".html>" + tmp.titolo + "</a>";
 				elem.setAttribute("onclick", "showPost(" + tmp.id + ")");
 				threadList.appendChild(elem);
 
@@ -29,7 +29,7 @@ function showPost(id) {
 			postList.innerHTML = "";
 			for (let tmp of r) {
 				let elem = document.createElement("li");
-				elem.innerHTML = "<br>"+ tmp.userPost.username + " <br> " + tmp.contenuto;
+				elem.innerHTML = "<br>" + tmp.userPost.username + " <br> " + tmp.contenuto;
 				let p = document.createElement("p");
 				p.id = tmp.id;
 				p.innerHTML = "Visualizza i commenti";
@@ -46,19 +46,21 @@ function showPost(id) {
 }
 
 function showLastPost() {
-	
+
 	fetch('http://localhost:8080/getAllOrderByDateAndTime').then((r) => { return r.json() })
 		.then((r) => {
 			let i = 1;
 			for (let tmp of r) {
-				let date = document.getElementById("date"+i);
-				date.innerHTML = "Scritto il "+tmp.date+" alle "+tmp.time;
-				let titolo = document.getElementById("titolo"+i);
+				let date = document.getElementById("date" + i);
+				date.innerHTML = "Scritto il " + tmp.date + " alle " + tmp.time;
+				let titolo = document.getElementById("titolo" + i);
 				titolo.innerHTML = tmp.titolo;
-				let contenuto = document.getElementById("contenuto"+i);
+				let contenuto = document.getElementById("contenuto" + i);
 				contenuto.innerHTML = tmp.contenuto;
-				let user = document.getElementById("user"+i);
-				user.innerHTML = "Scritto da "+tmp.user.username;
+				let user = document.getElementById("user" + i);
+				user.innerHTML = "Scritto da " + tmp.user.username;
+				let pComm = document.getElementById("commento" + i);
+				pComm.setAttribute("onclick", "commenta(" + tmp.id + ")");
 				i++;
 			}
 
@@ -80,4 +82,77 @@ function showCommenti(id) {
 			}
 
 		});
+}
+
+function commenta(id) {
+	let div = document.getElementById("aggiungiCommento");
+	div.innerHTML = "";
+	div.setAttribute("style", "margin-left:30%;");
+	var br = document.createElement("br");
+
+	var form = document.createElement("form");
+	form.setAttribute("id","formId");
+
+
+
+	var textarea = document.createElement("textarea");
+	textarea.setAttribute("id", "textarea");
+	textarea.setAttribute("placeholder", "Scrivi quello che cazzo voi");
+
+	var dateComm = document.createElement("input");
+	dateComm.setAttribute("type", "date");
+	dateComm.setAttribute("id", "dateComm");
+
+	var timeComm = document.createElement("input");
+	timeComm.setAttribute("type", "time");
+	timeComm.setAttribute("id", "timeComm");
+
+	var idComm = document.createElement("input");
+	idComm.setAttribute("type", "number");
+	idComm.setAttribute("id", "idUser");
+	idComm.setAttribute("placeholder", "Your user id");
+
+	var butt = document.createElement("input");
+	butt.setAttribute("type", "button");
+	butt.setAttribute("value", "Submit");
+	butt.setAttribute("onclick", "mandaCommento(" + id + ")");
+
+	form.appendChild(textarea);
+
+	form.appendChild(br.cloneNode());
+
+	form.appendChild(dateComm);
+	form.appendChild(br.cloneNode());
+
+	form.appendChild(timeComm);
+	form.appendChild(br.cloneNode());
+
+	form.appendChild(idComm);
+	form.appendChild(br.cloneNode());
+
+	form.appendChild(butt);
+
+	div.appendChild(form);
+}
+
+function mandaCommento(id) {
+	
+	let data={
+		contenuto: textarea.value,
+		date: dateComm.value,
+		time: timeComm.value+":00",
+		idUser: idUser.value,
+		idPost: id
+	}
+	
+	fetch('http://localhost:8080/createcommento', {
+  		method: 'POST',
+  		headers: {
+    		'Content-Type': 'application/json',
+  		},
+  		body: JSON.stringify(data),
+	}).then(
+		formId.style.visibility='hidden'
+	);
+	
 }
