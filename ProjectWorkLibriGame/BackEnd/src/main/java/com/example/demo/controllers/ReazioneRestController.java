8f7f1ca202918a5edad322cb1bcec404abb1f390
Croissanto <antoniopapaleo24@gmail.com>
2023-01-29 15:34:03 +0100

    sistemato il problema
    -rimosso il metodo della repository

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/controllers/ReazioneRestController.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/controllers/ReazioneRestController.java
@@ -1,12 +1,11 @@
 package com.example.demo.controllers;
 
-import java.util.LinkedList;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,16 +59,5 @@ public class ReazioneRestController {
 
 	}
 
-	@GetMapping("/getreazionebypost/{id}")
-	public List<Reazione> getReazioneByPostId(@PathVariable("id") int id) {
-
-		Iterable<Reazione> tmp = reazioneRepo.findByPostId(id);
-		List<Reazione> list = new LinkedList<>();
-		for (Reazione reazione : tmp) {
-
-			list.add(reazione);
-		}
-		return list;
-	}
-
+	
 }
