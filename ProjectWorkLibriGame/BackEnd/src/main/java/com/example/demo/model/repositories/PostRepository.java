2cf0dd96a738ff1fa06238015c5301c3d6367a1c
Croissanto <antoniopapaleo24@gmail.com>
2023-01-31 12:38:20 +0100

    Update reazioni
    -sistemata possibilità di reactare solo una volta per user ad ogni post

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/repositories/PostRepository.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/repositories/PostRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.Post;
 
+
 public interface PostRepository extends CrudRepository<Post, Integer> {
 
 	Iterable<Post> findTop2ByOrderByDateDescTimeDesc();
 
 	List<Integer> countReactionsById(int id);
+	
+	
 
 }
