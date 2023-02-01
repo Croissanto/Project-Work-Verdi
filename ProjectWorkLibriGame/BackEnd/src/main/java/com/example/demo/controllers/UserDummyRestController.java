3757876d81428b57a8a4707028e7ba0bb2713780
Croissanto <antoniopapaleo24@gmail.com>
2023-02-01 12:29:56 +0100

    Update UserDummyRestController.java
    -aggiunto metodo getAllUsers

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/controllers/UserDummyRestController.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/controllers/UserDummyRestController.java
@@ -1,20 +1,24 @@
 package com.example.demo.controllers;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.dto.CreateUserDummyDTO;
 import com.example.demo.model.UserDummy;
 import com.example.demo.model.repositories.UserDummyRepository;
 
 @RestController
 public class UserDummyRestController {
-	
+
 	@Autowired
 	private UserDummyRepository userDummyRepo;
 
-	
 	@PostMapping("/createuserdummy")
 	public boolean createAuthor(@RequestBody CreateUserDummyDTO dto) {
 		try {
@@ -27,6 +31,19 @@ public class UserDummyRestController {
 		}
 
 	}
-	
-	
+
+	@GetMapping("/getallusers")
+	public List<UserDummy> getUsers() {
+
+		Iterable<UserDummy> list = userDummyRepo.findAll();
+		for (UserDummy user : list) {
+
+			List<UserDummy> userlist = new LinkedList<>();
+			userlist.add(user);
+			return userlist;
+		}
+
+		return new LinkedList<>();
+	}
+
 }
