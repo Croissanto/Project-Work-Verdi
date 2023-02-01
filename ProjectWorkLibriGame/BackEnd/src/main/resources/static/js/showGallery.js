2943d3095e3c6f64ad3a75e6726bffd9b6ef464d
Biedr <pietrococozza1@gmail.com>
2023-02-01 22:10:25 +0100

    prendetene e mangetenenenenenene
    Sistemata html,css della gallery siuuuuuu

--- ProjectWorkLibriGame/BackEnd/src/main/resources/static/js/showGallery.js
+++ ProjectWorkLibriGame/BackEnd/src/main/resources/static/js/showGallery.js
@@ -13,12 +13,12 @@ function showAll() {
 		.then((r) => {
 			gallery.innerHTML = "";
 			for (let tmp of r) {
-				let elem = document.createElement("li");
+				let elem = document.createElement("a");
+				elem.setAttribute("class", "list-group-item list-group-item-action");
+				//role cambia il cursose quando passo sopra l'elemento
+				elem.setAttribute("role", "button");
 				elem.innerHTML = tmp.titoloSaga;
-				let button = document.createElement("button");
-				button.innerHTML = "Mostra Libri";
-				button.setAttribute("onclick", "showLibriGame(" + tmp.idGallery + ")");
-				elem.appendChild(button);
+				elem.setAttribute("onclick", "showLibriGame(" + tmp.idGallery + ")");
 				gallery.appendChild(elem);
 			}
 
@@ -28,6 +28,7 @@ function showLibriGame(id) {
 
 	fetch(`http://localhost:8083/getlibrigamebygalleryid/` + id).then((r) => { return r.json() })
 		.then((r) => {
+			poli.innerHTML = "";
 			showAll();
 			cardList.innerHTML = "";
 			salcio.innerHTML = "";
@@ -35,37 +36,43 @@ function showLibriGame(id) {
 
 				let card =
 					`
-				<div class="titolo">
-				     <h2 class = "h2">${tmp.title}</h2>
+				
+			      
+			      <div class="row pt-3">
+			      <div class="col-sm-4 offset-sm-2">
+                     <img class="card-img-top" src="${tmp.link}"
+							alt="Card image" style="width: 100%;">
+                  </div>
+			      <div class="col-sm-6 pe-5 d-flex justify-content-start align-items-center flex-column">
+			      <div class="titolo">
+				<p class = "h4 mb-0">${tmp.title}</p>
 			    </div>
-			      <div class="autore-libro">
-			      <p class = "nome-cognome-autore">${tmp.autore.name} ${tmp.autore.surname}
+			    <div class="autore-libro">
+			      <p class = "nome-cognome-autore text-muted fw-bold">${tmp.autore.name} ${tmp.autore.surname}
 			      </div>
-			      <div class="container mt-3">
-			      <div class = "card">
-			      <br><br><br><br>
-			      <div class = "stars">
-			         <input class="star star-5" id="5-${tmp.id}" type="radio" name="star" onclick="send(5,${tmp.id})" />
-							<label class="star star-5" for="5-${tmp.id}"></label>
-							<input class="star star-4" id="4-${tmp.id}" type="radio" name="star" onclick="send(4,${tmp.id})" />
-							<label class="star star-4" for="4-${tmp.id}"></label>
-							<input class="star star-3" id="3-${tmp.id}" type="radio" name="star" onclick="send(3,${tmp.id})" />
-							<label class="star star-3" for="3-${tmp.id}"></label>
-							<input class="star star-2" id="2-${tmp.id}" type="radio" name="star" onclick="send(2,${tmp.id})" />
-							<label class="star star-2" for="2-${tmp.id}"></label>
-							<input class="star star-1" id="1-${tmp.id}" type="radio" name="star" onclick="send(1,${tmp.id})" />
-							<label class="star star-1" for="1-${tmp.id}"></label>
-			      </div>
-			      <img class="card-img-top" src="${tmp.link}"
-							alt="Card image" style="width:100px">
-							<div class="card-body">
-							<h4 class="card-title">Descrizione</h4>
-							<div class="card-text">Qui ci va la descrizione del libro da capire come sistemarla non ho piu voglia.
-							</div>
-						</div>
-			   	    </div>
-					</div>
-				</div>`;
+			       <div class="row justify-content-center">
+                    <p class="h4">Descrizione</p>
+
+                    <p class="h5" style = "max-width:70%;">Qui ci va la descrizione del libro da capire come sistemarla non ho piu voglia.</p>
+
+
+                </div>
+			      <div class="mb-0">Vota sto cazzo de libro</div>
+                    <div class = "stars" id = "rating">
+                        <input class="star star-5" id="5-${tmp.id}" type="radio" name="star" onclick="send(5,${tmp.id})" />
+                               <label class="star star-5" for="5-${tmp.id}"></label>
+                               <input class="star star-4" id="4-${tmp.id}" type="radio" name="star" onclick="send(4,${tmp.id})" />
+                               <label class="star star-4" for="4-${tmp.id}"></label>
+                               <input class="star star-3" id="3-${tmp.id}" type="radio" name="star" onclick="send(3,${tmp.id})" />
+                               <label class="star star-3" for="3-${tmp.id}"></label>
+                               <input class="star star-2" id="2-${tmp.id}" type="radio" name="star" onclick="send(2,${tmp.id})" />
+                               <label class="star star-2" for="2-${tmp.id}"></label>
+                               <input class="star star-1" id="1-${tmp.id}" type="radio" name="star" onclick="send(1,${tmp.id})" />
+                               <label class="star star-1" for="1-${tmp.id}"></label>
+                     </div>
+                  </div>
+                </div>
+               `;
 				cardList.innerHTML += card;
 				galleryCard.innerHTML = "";
 			}
@@ -90,24 +97,24 @@ function send(s, id) {
 	});
 
 }
+
 function libriDellaSettimana() {
 
 	fetch(`http://localhost:8083/getalllibrigame`).then((r) => { return r.json() })
 		.then((r) => {
-			let salcio = document.getElementById("salcio");
+			let salcio = document.getElementById("salcio"),
+			    shuffled = r.sort(() => 0.5 - Math.random()),
+			    carousel = shuffled.slice(0, 5);
 			salcio.innerHTML = "";
-			let shuffled = r.sort(() => 0.5 - Math.random());
-			let carousel = shuffled.slice(0, 5);
 
 			for (i = 0; i < carousel.length; i++) {
 				if (i < 1) {
 
 					let caroActive = `
 					
-    <div class="carousel-item active d-flex justify-content-center">
-      <img src="${carousel[i].link}" class="d-block w-100 " alt="...">
+    <div class="carousel-item active">
+      <img src="${carousel[i].link}" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>${carousel[i].title}</h5>
         <p></p>
       </div>
     </div>
@@ -123,7 +130,6 @@ function libriDellaSettimana() {
     <div class="carousel-item justify-content-center ">
       <img src="${carousel[i].link}" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>${carousel[i].title}</h5>
         <p></p>
       </div>
     </div>
@@ -141,37 +147,36 @@ function libriDellaSettimana() {
 
 
 function showGenre() {
-	var select = document.getElementById("genre");
-	var selectedValue = select.options[select.selectedIndex].value;
-
+	let select = document.getElementById("genre"), 
+		selectedValue = select.options[select.selectedIndex].value;
+	
 	fetch(`http://localhost:8083/getlibrigamebygenre/` + selectedValue).then((r) => { return r.json() })
 		.then((r) => {
 			micheal.innerHTML = "";
-
+            poli.innerHTML = "";
 			genreList.innerHTML = "";
 			for (let tmp of r) {
 				console.log(tmp);
 				let card =
 					`
-				<div class="titolo">
-				     <h2 class = "h2">${tmp.title}</h2>
+			<div class="row pt-3">
+			      <div class="col-sm-4 offset-sm-2">
+                     <img class="card-img-top" src="${tmp.link}"
+							alt="Card image" style="width: 100%;">
+                  </div>
+			      <div class="col-sm-6 pe-5 d-flex justify-content-start align-items-center flex-column">
+			      <div class="titolo">
+				<p class = "h4 mb-0">${tmp.title}</p>
 			    </div>
-			      <div class="autore-libro">
-			      <p class = "nome-cognome-autore">${tmp.autore.name} ${tmp.autore.surname}
+			    <div class="autore-libro">
+			      <p class = "nome-cognome-autore text-muted fw-bold">${tmp.autore.name} ${tmp.autore.surname}
 			      </div>
-			      <div class="container mt-3">
-			      <div class = "card">
-			      <br><br><br><br>
-			      <img class="card-img-top" src="${tmp.link}"
-							alt="Card image" style="width:100px">
-							<div class="card-body">
-							<h4 class="card-title">Descrizione</h4>
-							<div class="card-text">Qui ci va la descrizione del libro da capire come sistemarla non ho piu voglia.
-							</div>
-						</div>
-			   	    </div>
-					</div>
-				</div>`;
+			       <div class="row justify-content-center border-top border-bottom">
+                 
+                    <p class="h5" style = "max-width:70%;">Qui ci va la descrizione del libro da capire come sistemarla non ho piu voglia.</p>
+
+
+                </div>`;
 
 				genreList.innerHTML += card;
 				cardList.innerHTML = "";
