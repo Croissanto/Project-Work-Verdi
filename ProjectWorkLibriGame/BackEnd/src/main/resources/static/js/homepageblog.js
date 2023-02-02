f708543b8c38e12f78616492af20cac8c619f164
Antonio Tallo <tallo.antonio1998@gmail.com>
2023-02-02 09:58:56 +0100

    In homepage mostra gli ultimi 2 post

--- ProjectWorkLibriGame/BackEnd/src/main/resources/static/js/homepageblog.js
+++ ProjectWorkLibriGame/BackEnd/src/main/resources/static/js/homepageblog.js
@@ -1,20 +1,25 @@
 document.body.onload = () => {
-	
+
 	showTop2();
-	
+
 }
 
 function showTop2() {
-	
-	fetch('http://localhost:8083/gettop2recentposts').then((r) => {return r.json()}).then((r) => {
-		
-		
-		for(let post of r) {
-		
-		let elem = document.createElement("li")
-		 elem.innerHTML = post.titolo + ' '+ post.contenuto + ' Postato da '+post.user.username;
-		
-		ul.appendChild(elem);
+
+	fetch('http://localhost:8083/gettop2recentposts').then((r) => { return r.json() }).then((r) => {
+
+		let i = 1;
+		for (let post of r) {
+			let data = document.getElementById("dateMostU" + i);
+			data.innerHTML = post.date + " alle " + post.time;
+			let titolo = document.getElementById("titoloMostU" + i);
+			titolo.innerHTML = post.titolo;
+			let contenuto = document.getElementById("contenutoMostU" + i);
+			contenuto.innerHTML = post.contenuto;
+			let user = document.getElementById("userMostU" + i);
+			user.innerHTML = post.user.username;
+			
+			i++;
 		}
 	});
 
