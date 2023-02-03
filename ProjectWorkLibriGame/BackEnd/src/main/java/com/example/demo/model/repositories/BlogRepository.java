8774d2fc962241623faf4b001d15ab3e2b563419
Antonio Tallo <tallo.antonio1998@gmail.com>
2023-02-03 12:39:42 +0100

    Fateli bene sti cazzo di collegamenti

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/repositories/BlogRepository.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/repositories/BlogRepository.java
@@ -1,12 +1,16 @@
 package com.example.demo.model.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.Blog;
+import com.example.demo.model.Post;
 
 public interface BlogRepository extends CrudRepository<Blog, Integer> {
 	
 	Optional<Blog> findById(int id);
+	
+	
 }
