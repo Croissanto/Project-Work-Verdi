e794baad92be60f57c13351709e0bee8df71a017
Biedr <pietrococozza1@gmail.com>
2023-02-03 10:51:18 +0100

    Create funzione avarage rating
    Fatta

--- ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/repositories/RatingRepository.java
+++ ProjectWorkLibriGame/BackEnd/src/main/java/com/example/demo/model/repositories/RatingRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.model.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.demo.model.IMostRated;
 import com.example.demo.model.LibriGame;
 import com.example.demo.model.Rating;
 import com.example.demo.model.UserDummy;
@@ -11,7 +14,9 @@ import com.example.demo.model.UserDummy;
 public interface RatingRepository extends CrudRepository<Rating, Integer> {
 	boolean existsRatingByUserAndLibri(UserDummy user,LibriGame libri);
 	Optional<Rating> findByUserAndLibri(UserDummy user,LibriGame libri);
-	
 	Iterable<Rating> findByOrderByStarDesc();
-
+     @Procedure("most_rated")
+     List<IMostRated> avg();
+     
+	
 }
